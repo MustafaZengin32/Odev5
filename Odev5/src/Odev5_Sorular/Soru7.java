203178881506420c5da9d8fc54723186ada7de39
Jasi <Jasi@Asi1987>
2022-12-23 23:27:20 +0100

    odev5A

--- Odev5/src/Odev5_Sorular/Soru7.java
+++ Odev5/src/Odev5_Sorular/Soru7.java
@@ -1,9 +1,37 @@
 package Odev5_Sorular;
 
+import java.util.Scanner;
+
 public class Soru7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Kullanicidan toplamak uzere pozitif sayilar isteyin, 
+				///islemi bitirmek icin 0’a basmasinisoyleyin. 
+				//Kullanici 0’a bastiginda toplam kac pozitif sayi girdigini ve
+				//girdigi pozitif sayilarin toplaminin kac oldugunu yazdirin.(do while dongusu ile)
+				
+				
+				Scanner scan=new Scanner(System.in);
+				System.out.println("Pozitif sayi girin");
+				
+				
+				int a=0;
+				int sayac=0;
+				int sayi=0;
+				
+				do {
+					sayi=scan.nextInt();
+					
+					
+					if(sayi>0) {
+						a=a+sayi;
+						sayac++;
+					}
+					
+				} 
+				while (sayi!=0);
+				System.out.println(a);
+				System.out.println(sayac);
 
 	}
 
--- Odev5/src/Odev5_Sorular/Soru9.java
+++ Odev5/src/Odev5_Sorular/Soru9.java
@@ -1,9 +1,28 @@
 package Odev5_Sorular;
 
+import java.util.Scanner;
+
 public class Soru9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Kullanicidan pozitif bir rakam girmesini isteyin ve girilen rakama gore carpim tablosu olusturun.
+				//Ornek,kullanici 3 girerse,
+				//1 2 3
+				//2 4 6
+				//3 6 9
+				
+				Scanner scan=new Scanner(System.in);
+				System.out.println("sayi girin");
+				int sayi=scan.nextInt();
+				int carpim=1;
+				for (int i = 1; i <=sayi; i++) {
+					for (int j = 1; j <=sayi; j++) {
+				
+						System.out.print(i*j+" ");
+					}
+					System.out.println();
+				}
 
 	}
 
